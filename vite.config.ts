9c0bfe1ad41631aee5aa2535b428260d8c0db9ef
notut <noratt66@gmail.com>
2025-03-28 11:21:46 +0100

    lagt til index fil og vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/exercise-5",
+  /*server: {
+        proxy: {
+            "/exercise-5/api": "http://localhost:3000",
+        },
+    },*/
+});
