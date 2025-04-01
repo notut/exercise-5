3967a020b828f68336d6b721496593663c028751
notut <noratt66@gmail.com>
2025-04-01 13:41:11 +0200

    oppdatering av kart, skoler og kommuner

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,15 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+//import pg from "pg";
+
+//const postgresql = new pg.Pool({ user: "postgres" });
 
 const app = new Hono();
 app.get("/", async (c) => {
   return c.text("Heisann");
 });
 app.get("exercise-5/api/kommuner_grense.json", (c) => {
+  //const result = postgresql.query("select");
   return c.json({
     type: "FeatureCollection",
     crs: {
--- src/modules/application.tsx
+++ src/modules/application.tsx
@@ -1,92 +1,37 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
+
+import "ol/ol.css";
+import { GeoJSON } from "ol/format";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
-import GeoJSON from "ol/format/GeoJSON";
-import { useGeographic } from "ol/proj";
-import { Style, Fill, Stroke } from "ol/style";
-import Overlay from "ol/Overlay";
-import "ol/ol.css";
 
 useGeographic();
 
+const osmLayer = new TileLayer({ source: new OSM() });
+const municipalityLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "exercise-5/geojson/kommuner_grense.json",
+    format: new GeoJSON(),
+  }),
+});
+const schoolLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "exercise-5/geojson/Videreg_ende skoler.json",
+    format: new GeoJSON(),
+  }),
+});
+
+const map = new Map({
+  view: new View({ center: [10.8, 59.9], zoom: 8 }),
+  layers: [osmLayer, municipalityLayer, schoolLayer],
+});
+
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
-  const popupRef = useRef<HTMLDivElement | null>(null);
-  const [popupInfo] = useState<{
-    name: string;
-    coordinates: number[];
-  } | null>(null);
-
-  //Stil for fylkene
-  useEffect(() => {
-    const defaultPolygonStyle = new Style({
-      fill: new Fill({ color: "rgba(245,33,233,0.3)" }),
-      stroke: new Stroke({ color: "#f85699", width: 2 }),
-    });
-
-    //Hover-stil for fylkene
-    const hoverPolygonStyle = new Style({
-      fill: new Fill({ color: "rgba(0,200,255,0.5)" }),
-      stroke: new Stroke({ color: "#00BBFF", width: 3 }),
-    });
-
-    //Henter oversikt over fylkene
-    const KommunerSource = new VectorSource({
-      url: "exercise-5/api/kommuner_grense.json",
-      format: new GeoJSON(),
-    });
-
-    const KommunerLayer = new VectorLayer({
-      source: KommunerSource,
-      style: defaultPolygonStyle,
-    });
-
-    //Popup med informasjon
-    const popupOverlay = new Overlay({
-      element: popupRef.current!,
-      positioning: "bottom-center",
-      stopEvent: false,
-      offset: [0, -10],
-    });
-
-    //Oppretter kartet
-    const map = new Map({
-      target: mapRef.current!,
-      view: new View({ center: [10.8, 59.9], zoom: 6 }),
-      layers: [new TileLayer({ source: new OSM() }), KommunerLayer],
-      overlays: [popupOverlay],
-    });
-
-    //Viser popup med klikk
-    map.on("pointermove", (event) => {
-      const feature = map.forEachFeatureAtPixel(event.pixel, (feat) => feat);
-      KommunerLayer.getSource()
-        ?.getFeatures()
-        .forEach((feat) => {
-          feat.setStyle(
-            feat === feature ? hoverPolygonStyle : defaultPolygonStyle,
-          );
-        });
-    });
-    return () => {
-      map.setTarget(undefined);
-    };
-  }, []);
-
-  //Stil på pop up beskjed
-  return (
-    <div className="map-container">
-      <div ref={mapRef} style={{ width: "100%", height: "100%" }} />
-      <div
-        ref={popupRef}
-        className="popup"
-        style={{ display: popupInfo ? "block" : "none" }}
-      >
-        {popupInfo && <div className="popup-content">{popupInfo.name}</div>}
-      </div>
-    </div>
-  );
+  useEffect(() => map.setTarget(mapRef.current!), []);
+  return <div ref={mapRef}></div>;
 }
