93712f1ba4a901900918935f648d6998a2fee7a9
notut <noratt66@gmail.com>
2025-04-01 10:33:06 +0200

    start av geojson fil

--- server/server.ts
+++ server/server.ts
@@ -5,4 +5,16 @@ const app = new Hono();
 app.get("/", async (c) => {
   return c.text("Heisann");
 });
+app.get("exercise-5/api/kommuner_grense.json", (c) => {
+  return c.json({
+    type: "FeatureCollection",
+    crs: {
+      type: "name",
+      properties: {
+        name: "urn:ogc:def:crs:OGC:1.3:CRS84",
+      },
+    },
+    features: [],
+  });
+});
 serve(app);
