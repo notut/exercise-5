efb2a070ab7b5146b88604f950072b599d5c9e4b
notut <noratt66@gmail.com>
2025-04-04 11:23:25 +0200

    database og server fil + vite.config - hentet informasjon fra databasen til API

--- server/server.ts
+++ server/server.ts
@@ -1,24 +1,44 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
-//import pg from "pg";
+import pg from "pg";
 
-//const postgresql = new pg.Pool({ user: "postgres" });
+const postgresql = new pg.Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "postgres",
+  port: 5432,
+});
 
 const app = new Hono();
 app.get("/", async (c) => {
   return c.text("Heisann");
 });
-app.get("exercise-5/api/kommuner_grense.json", (c) => {
-  //const result = postgresql.query("select");
+// Henter skoler som GeoJSON
+app.get("/exercise-5/api/skoler", async (c) => {
+  console.log("Kjører SQL-spørring...");
+  const result = await postgresql.query(
+    "SELECT skolenavn, posisjon::json as coordinates FROM grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole",
+  );
+  console.log("Resultat hentet, lager GeoJSON");
+
+  const features = result.rows.map((row) => ({
+    type: "Feature",
+    properties: {
+      skolenavn: row.skolenavn,
+    },
+    geometry: {
+      type: "Point",
+      coordinates: row.coordinates.coordinates,
+    },
+  }));
+
   return c.json({
     type: "FeatureCollection",
-    crs: {
-      type: "name",
-      properties: {
-        name: "urn:ogc:def:crs:OGC:1.3:CRS84",
-      },
-    },
-    features: [],
+    features,
   });
 });
+
+// Start server
 serve(app);
+console.log("Server kjører på http://localhost:3000");
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/exercise-5",
-  /*server: {
-        proxy: {
-            "/exercise-5/api": "http://localhost:3000",
-        },
-    },*/
+  server: {
+    proxy: {
+      "/exercise-5/api": "http://localhost:3000",
+    },
+  },
 });
