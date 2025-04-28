888036e312a1731b89f8ad13217d754e29419701
notut <noratt66@gmail.com>
2025-04-28 10:42:19 +0200

    oppdatert

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Application } from "./modules/application";
+import { Application } from "./modules/app/application";
 
 const rootElement = document.getElementById("root");
 
